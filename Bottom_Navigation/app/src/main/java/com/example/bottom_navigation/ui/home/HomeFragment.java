0d0424ca2b52825c14f6f5e27e28afd3762c959d
raymond <rholidjaja@gmail.com>
2019-11-04 20:52:34 -0600

    Initial Commit

---
+++ Bottom_Navigation/app/src/main/java/com/example/bottom_navigation/ui/home/HomeFragment.java
@@ -0,0 +1,79 @@
+package com.example.bottom_navigation.ui.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+ import com.example.bottom_navigation.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PointOfInterest;
+
+public class HomeFragment extends Fragment implements OnMapReadyCallback {
+
+    private HomeViewModel homeViewModel;
+    private GoogleMap mMap;
+
+    SupportMapFragment mapFragment;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        homeViewModel =
+                ViewModelProviders.of(this).get(HomeViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_home, container, false);
+        mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+
+        if(mapFragment == null) {
+            FragmentManager fm = getFragmentManager();
+            FragmentTransaction ft = fm.beginTransaction();
+            mapFragment = SupportMapFragment.newInstance();
+            ft.replace(R.id.map, mapFragment).commit();
+        }
+        //
+//        final TextView textView = root.findViewById(R.id.text_home);
+//        homeViewModel.getText().observe(this, new Observer<String>() {
+//            @Override
+//            public void onChanged(@Nullable String s) {
+//                textView.setText(s);
+//            }
+//        });
+        mapFragment.getMapAsync(this);
+        return root;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng coffman = new LatLng(44.9728134, -93.2353374);
+        mMap.addMarker(new MarkerOptions().position(coffman).title("Marker in coffman"));
+
+        LatLng umn = new LatLng(44.973086, -93.2370881);
+
+        mMap.setOnPoiClickListener(new GoogleMap.OnPoiClickListener() {
+            @Override
+            public void onPoiClick(PointOfInterest pointOfInterest) {
+                mMap.addMarker(new MarkerOptions().position(pointOfInterest.latLng).title("22")).showInfoWindow();
+            }
+        });
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(coffman));
+        mMap.animateCamera(CameraUpdateFactory.zoomIn());
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(16), 2000, null);
+    }
+}
