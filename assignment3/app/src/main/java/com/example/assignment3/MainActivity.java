88b48af206659d517b4c9b9a6f1a5a997ba26310
Lbarnhart <barnh043@umn.edu>
2019-11-10 03:17:00 -0600

    Add files via upload

--- assignment3/app/src/main/java/com/example/assignment3/MainActivity.java
+++ assignment3/app/src/main/java/com/example/assignment3/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 import android.content.Intent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -74,15 +75,14 @@ public class MainActivity extends AppCompatActivity {
 
         //setting adapter to recyclerview
         recyclerView.setAdapter(adapter);
-    }
 
-    public void sendMessage(View view)
-    {
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.button);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
-    }
+        Button mButton = (Button) findViewById(R.id.button);
+        mButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                startActivity(new Intent(MainActivity.this, Report_Form.class));
+            }
+        });
 
+    }
 }
